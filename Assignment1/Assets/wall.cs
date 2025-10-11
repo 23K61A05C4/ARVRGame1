2a88edd327905e6d3faa005ffc52fed95575ccc2
Nagaboyina Jahnavi <jahnavi.nagaboyina@sasi.ac.in>
2025-10-11 13:54:54 +0530

    UPDATE

---
+++ Assignment1/Assets/wall.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wall : MonoBehaviour
+{
+    public MeshRenderer m_Renderer;
+    private Rigidbody rb;
+    private void Start()
+    {
+        m_Renderer = GetComponent<MeshRenderer>();
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "player")
+        {
+            m_Renderer.material.color = Color.red;
+        }
+
+    }
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "player")
+        {
+            m_Renderer.material.color = Color.blue;
+        }
+    }
+}
